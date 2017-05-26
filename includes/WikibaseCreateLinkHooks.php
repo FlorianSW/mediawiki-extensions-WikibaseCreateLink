c8d6f63ad6ffa4ec38f89cd8a82b6c0046ec845f
Florian Schmidt <florian.schmidt.stargatewissen@gmail.com>
2017-05-26 20:56:51 +0200

    Initial commit

---
+++ includes/WikibaseCreateLinkHooks.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace WikibaseCreateLink;
+
+use Wikibase\Client\WikibaseClient;
+use BaseTemplate;
+use Exception;
+
+class Hooks {
+	public static function setup() {
+		if ( !class_exists( 'WikibaseClient' ) ) {
+			throw new Exception( 'The Wikibase Client extension needs to be ' .
+				'installed to work with this extension.' );
+		}
+	}
+
+	/**
+	 * Add "Create Wikibase item" link in toolbox
+	 *
+	 * @param BaseTemplate $baseTemplate
+	 * @param array $toolbox
+	 */
+	public static function onBaseTemplateToolbox( BaseTemplate $baseTemplate, array &$toolbox ) {
+		$wikibaseClient = WikibaseClient::getDefaultInstance();
+		$skin = $baseTemplate->getSkin();
+		$title = $skin->getTitle();
+		$idString = $skin->getOutput()->getProperty( 'wikibase_item' );
+		if ( !$idString ) {
+			$repoLinker = $wikibaseClient->newRepoLinker();
+			$createItemPage = $repoLinker->getPageUrl(
+				\SpecialPage::getTitleFor( 'NewItem' )->getFullText()
+			);
+			$createItemPage = $repoLinker->addQueryParams(
+				$createItemPage,
+				[
+					'lang' => $skin->getLanguage()->getCode(),
+					'label' => $title->getPrefixedText(),
+				]
+			);
+			$toolbox['wikibasecreatelink'] = array(
+				'text' => $baseTemplate->getMsg( 'wikibasecreatelink-createlabel' )->text(),
+				'href' => $createItemPage,
+				'id' => 't-wikibasecreatelink'
+			);
+		}
+	}
+}
